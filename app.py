e5ff0f2118b53d52907e48fcf12b4b8f3286eff5
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-03-03 12:44:54 +0530

    add SQLAlchemy integration and create Todo model with database setup

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    content = db.Column(db.String(200),nullable=False)
+    completed = db.Column(db.Integer,default=0)
+    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
+    with app.app_context():  # Ensure database tables are created
+        db.create_all()
+        print("Database created successfully!")
     app.run(debug=True)
