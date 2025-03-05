5ec13a7176997da15321e5a3c5536067da2d9a48
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-03-05 21:29:26 +0530

    format the code

--- app.py
+++ app.py
@@ -6,16 +6,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    content = db.Column(db.String(200),nullable=False)
-    completed = db.Column(db.Integer,default=0)
-    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/',methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
@@ -29,8 +31,8 @@ def index():
             return "There was an issue adding your task"
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html',tasks = tasks)
-    
+        return render_template('index.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -42,8 +44,9 @@ def delete(id):
         return redirect('/')
     except:
         return "There was a problem deleting that task"
-    
-@app.route('/update/<int:id>',methods=['GET','POST'])
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
@@ -57,6 +60,7 @@ def update(id):
             return "There was an issue updating your task"
     else:
         return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
     with app.app_context():  # Ensure database tables are created
